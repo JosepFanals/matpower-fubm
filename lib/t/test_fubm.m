e446ba299b817cebe56ba4afe93ce612e173c80e
AbrahamAlvarezB <snoop_and@hotmail.com>
2020-05-26 14:54:00 +0100

    Power Flow t_pf_acdc_fubm

--- lib/t/test_fubm.m
+++ lib/t/test_fubm.m
@@ -1,8 +1,8 @@
 function [results, iBeqz, iBeqv] = test_fubm
-%% TEST_FUBM Tests the FUBM Derivatives against Finite Differences Method
+%% TEST_FUBM Tests FUBM 
 clear all;
 clc; 
 
 t_jacobian_fubm;
 t_hessian_fubm;
-
+t_pf_acdc_fubm;
